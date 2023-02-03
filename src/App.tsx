4e051b6a70b4413872233d13fed0d2bfa06d1d6a
h12j21-star <jh06079@naver.com>
2023-02-03 11:49:38 +0900

    data 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import NavBar from "./components/common/NavBar";
+import Router from "./routes/Router";
+
+export default function App() {
+  // 서버에 데이터 요청
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Router />
     </div>
   );
 }
-
-export default App;
---
+++ src/data.tsx
@@ -0,0 +1,44 @@
+let data = [
+  {
+    id: 0,
+    title: "호박이 케이스1",
+    content: "호박이의 자유로운 모습을 담아낸 케이스 입니다.",
+    price: 12000,
+    image: "case1.jpg",
+  },
+  {
+    id: 1,
+    title: "호박이 케이스2",
+    content: "호박이의 자유로운 모습을 담아낸 케이스 입니다.",
+    price: 12000,
+    image: "case2.jpg",
+  },
+
+  {
+    id: 2,
+    title: "호박이 케이스3",
+    content: "겨울에 어울리는 호박이 케이스 입니다.",
+    price: 10000,
+    image: "case3.jpg",
+  },
+  {
+    id: 3,
+    title: "호박이 키링",
+    content: "귀여운 호박이 키링입니다.",
+    price: 10000,
+  },
+  {
+    id: 4,
+    title: "호박이 달력",
+    content: "Born in Seoul",
+    price: 110000,
+  },
+
+  {
+    id: 6,
+    title: "호박이 반팔",
+    content: "Born in the States",
+    price: 130000,
+  },
+];
+export default data;
