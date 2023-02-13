d713139de60e44447ff038d9de5b7c46b7e36d24
h12j21-star <jh06079@naver.com>
2023-02-13 13:20:10 +0900

    section heading태그 추가 #3

--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -1,31 +1,33 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router";
 export default function NavBar() {
   let navigate = useNavigate();
   return (
     <>
-      <Navbar bg="light" variant="light">
-        <Container>
-          <Navbar.Brand href="#home">DIDI0Book</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link
-              onClick={() => {
-                navigate("/");
-              }}
-            >
-              HOME
-            </Nav.Link>
-            <Nav.Link
-              onClick={() => {
-                navigate("/");
-              }}
-            >
-              My Book List
-            </Nav.Link>
-           
-          </Nav>
-        </Container>
-      </Navbar>
+      <header>
+        <h1 className="ir">도서 검색 사이트</h1>
+        <Navbar bg="light" variant="light">
+          <Container>
+            <Navbar.Brand href="#home">DIDI0Book</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link
+                onClick={() => {
+                  navigate("/");
+                }}
+              >
+                HOME
+              </Nav.Link>
+              <Nav.Link
+                onClick={() => {
+                  navigate("/");
+                }}
+              >
+                My Book List
+              </Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </header>
       <div className="main-bg"></div>
     </>
   );
